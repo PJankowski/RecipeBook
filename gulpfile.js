2185077669a76c8a71bd9c4ef78d22c1a60f9ec0
Paul Jankowski <PJankowski25@gmail.com>
2015-10-26 15:57:16 -0400

    Put clean gulp task back in

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('clean', function(){
   return del(['dist/**/*']);
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', ['clean'], function() {
     gulp.src('src/sass/main.sass')
       .pipe(sass())
       .pipe(gulp.dest('src/stylesheets'));
